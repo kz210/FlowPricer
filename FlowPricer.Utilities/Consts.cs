bd8ac1da8eb2e742c28ddce32b95fcfb95218a43
kun zhao <kun.zhao@hotmail.co.uk>
2020-02-08 01:15:35 +0000

    FlowPricer First Commit to push to git

---
+++ FlowPricer.Utilities/Consts.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowPricer.Utilities
+{
+    public static class Consts
+    {
+        public static double[] CDSTenor =  { 1d, 2d, 3d, 4d, 5d, 7d, 10d };
+
+    }
+}
---
+++ FlowPricer/Ribbon1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace FlowPricer
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorksheet();
+            currentSheet.Range["A1"].Value = "Hello World";
+            currentSheet.Columns.AutoFit();
+        }
+
+        private void button1_Click_1(object sender, RibbonControlEventArgs e)
+        {
+            Worksheet currentSheet = Globals.ThisAddIn.GetActiveWorksheet();
+            currentSheet.Range["A3"].Value = "100 %";
+            currentSheet.Columns.AutoFit();
+        }
+    }
+}
